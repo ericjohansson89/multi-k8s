32acf34ef56fa8f5377093610678b1cfe94d1c9f
Eric Johansson <erijoh@mac-se-erijoh.local>
2020-01-02 11:27:33 +0100

    Adding logging

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
+echo "starting to build images"
 docker build -t ericsojohansson/multi-client:latest -t ericsojohansson/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t ericsojohansson/multi-server:latest -t ericsojohansson/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t ericsojohansson/multi-worker:latest -t ericsojohansson/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+echo "starting to push images"
 docker push ericsojohansson/multi-client:lastest
 docker push ericsojohansson/multi-server:latest
 docker push ericsojohansson/multi-worker:latest
 docker push ericsojohansson/multi-client:$SHA
 docker push ericsojohansson/multi-server:$SHA
 docker push ericsojohansson/multi-worker:$SHA
+echo "applying images"
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ericsojohansson/multi-server:$SHA
 kubectl set image deployments/client-deployment server=ericsojohansson/multi-client:$SHA
