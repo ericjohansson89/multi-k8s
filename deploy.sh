276f7099b52f888f1722d8eeb0a85fe7608ea7cc
Eric Johansson <erijoh@mac-se-erijoh.local>
2020-01-02 12:18:55 +0100

    more fixes

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ docker build -t ericsojohansson/multi-client:latest -t ericsojohansson/multi-cli
 docker build -t ericsojohansson/multi-server:latest -t ericsojohansson/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t ericsojohansson/multi-worker:latest -t ericsojohansson/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 echo "starting to push images"
-docker push ericsojohansson/multi-client:lastest
+docker push ericsojohansson/multi-client:latest
 docker push ericsojohansson/multi-server:latest
 docker push ericsojohansson/multi-worker:latest
 docker push ericsojohansson/multi-client:$SHA
@@ -12,5 +12,5 @@ docker push ericsojohansson/multi-worker:$SHA
 echo "applying images"
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ericsojohansson/multi-server:$SHA
-kubectl set image deployments/client-deployment server=ericsojohansson/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=ericsojohansson/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=ericsojohansson/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ericsojohansson/multi-worker:$SHA
